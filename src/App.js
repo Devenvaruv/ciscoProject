2ea912234ad5670dc203465c14d406185654d91b
your_github_username <devenvaruv@gmail.com>
2024-11-13 15:32:49 -0800

    added new sprite and new animation

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ function App() {
     setIsLoading(true);
 
     try {
-
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await new Promise((resolve) => setTimeout(resolve, 7000));
 
       const fetchedDashboardUrl =
         "https://play.grafana.org/d/play-bigquery-1/citibike-example-overview?orgId=1&from=2017-04-01T00:00:00.000Z&to=2018-05-31T23:59:59.000Z&timezone=utc";
@@ -35,7 +34,6 @@ function App() {
 
   const handleInputChange = (event) => {
     setChatInput(event.target.value);
-    
   };
 
   return (
@@ -44,6 +42,18 @@ function App() {
         <div className="chat-content">
           {isLoading ? (
             <div className="loading-indicator">
+              <div class="logo-animation">
+                <img src="/images/Sprite-smallest.png" alt="Bar 1" class="bar" />
+                <img src="/images/Sprite-small.png" alt="Bar 2" class="bar" />
+                <img src="/images/Sprite.png" alt="Bar 3" class="bar" />
+                <img src="/images/Sprite-small.png" alt="Bar 4" class="bar" />
+                <img src="/images/Sprite-smallest.png" alt="Bar 5" class="bar" />
+                <img src="/images/Sprite-small.png" alt="Bar 6" class="bar" />
+                <img src="/images/Sprite.png" alt="Bar 7" class="bar" />
+                <img src="/images/Sprite-small.png" alt="Bar 8" class="bar" />
+                <img src="/images/Sprite-smallest.png" alt="Bar 9" class="bar" />
+              </div>
+
               <h2>Loading...</h2>
             </div>
           ) : dashboardUrl ? (
@@ -55,15 +65,9 @@ function App() {
           ) : (
             <div className="placeholder">
               <div>
-              <h2>Cisco Intelligent Dashboard</h2>
-              <h3>Your data insights, simplified.</h3>
+                <h2>Cisco Intelligent Dashboard</h2>
+                <h3>Your data insights, simplified.</h3>
               </div>
-              {/* <img
-                src="https://aurus5.com/upload/blog/images/2017-cisco-logo-4.png"
-                
-                alt="Dashboard Placeholder"
-                className="placeholder-image"
-              /> */}
             </div>
           )}
         </div>
@@ -74,8 +78,13 @@ function App() {
             value={chatInput}
             onChange={handleInputChange}
             placeholder="Type your message..."
+            disabled={isLoading}
           />
-          <button className="chat-submit-button" type="submit">
+          <button
+            className="chat-submit-button"
+            type="submit"
+            disabled={isLoading}
+          >
             Send
           </button>
         </form>
